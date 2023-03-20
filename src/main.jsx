75857451465ba302b63cbc168fddcbce3555e4d1
Brian Pérez <115020892+Elvendetacos@users.noreply.github.com>
2023-03-19 19:01:27 -0600

    Revert "Feature/bp front 3 2"

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import landing from './pages/landingPage'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <landing/>
-)
-    
