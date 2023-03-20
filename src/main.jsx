4d3b77e76ef41a60f0fd3b8c24ff2ff33b8f75e0
BlackRubick <cesargomez_aguilera@hotmail.com>
2023-03-19 18:39:48 -0600

    Feature/BP FRONT 3-2

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Refills from './pages/refills'
+import landing from './pages/landingPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Refills />
+    <landing/>
 )
+    
