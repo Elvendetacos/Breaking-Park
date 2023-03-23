b92cdaf7f1ec1882b7ce26f598e91bd87a61c4ac
Abril Nuñez <abriln2830@gmail.com>
2023-03-22 21:51:20 -0600

    BP-FRONT-1-1: Sing-Up and Sing-In

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LandingPage from './pages/cancelReservation'
+import LandingPage from './pages/landingPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
