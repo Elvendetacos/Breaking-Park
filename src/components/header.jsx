967ca44d9f6abb07d263068d18028920c40d0fe3
BlackRubick <cesargomez_aguilera@hotmail.com>
2023-03-23 21:42:07 -0600

    Feature/BP-FRONT-3-3 Home

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ function Header({children}) {
         <>
         <header className="bg-[#263744] h-[100px] w-full font-bolder text-white text-[45px] flex justify-center items-center">
             <div className="h-full w-full ml-[32px] mr-[32px] flex justify-center items-center">
-                <div className="grid w-full lg:grid-cols-12 lg:gap-8 h-3/4 sm:grid-cols-4 sm:gap-4">
+                <div className="grid w-full lg:grid-cols-12 lg:gap-8 h-3/4 grid-cols-4 gap-4">
                 {children}
                 </div>
             </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,46 +1,80 @@
 import Header from "../components/header";
 import Body from "../components/body";
 import moneda from "../assets/img/coin.svg";
-import carro from "../assets/img/car-front.svg"
-import tacha from "../assets/img/x-circle.svg"
+import carro from "../assets/img/car-front.svg";
+import tacha from "../assets/img/x-circle.svg";
+
 function Home() {
+  const hola = () => {
+    console.log("hola pendejo1");
+  };
+  const hola2 = () => {
+    console.log("hola pendejo2");
+  };
+  const hola3 = () => {
+    console.log("hola pendejo3");
+  };
+
   return (
     <>
       <Header>
-        <div className="lg:col-span-3 lg:col-start-1">
-          <button>Breakig Park</button>
+        <div className="lg:col-span-3 lg:col-start-1 lg:mr-auto flex justify-center items-center">
+          <button className="text-lg lg:text-4xl lg:font-bold">
+            Breakig Park
+          </button>
         </div>
-        <div className="lg:col-span-2 lg:col-start-11 text-center">
-          <button>Registar</button>
+        <div className="lg:col-span-2 lg:col-start-11 text-center flex justify-center items-center col-start-3">
+          <button className="text-lg lg:text-4xl lg:font-bold">Registar</button>
         </div>
-        <div className="lg:col-span-2 lg:col-start-9 lg:row-start-1 text-right">
-          <button>Reservar</button>
+        <div className="lg:col-span-2 lg:col-start-9 lg:row-start-1 text-right flex justify-center items-center col-start-4">
+          <button className="text-lg lg:text-4xl lg:font-bold">Reservar</button>
         </div>
       </Header>
+
       <Body>
-        <div className="bg-[#171B26]">
-          <div className=" lg:ml-8 lg:mr-8">
-            <div className="grid lg:grid-cols-12 lg:h-[748px] lg:grid-rows-6 lg:gap-8  sm:grid-cols-4 sm:gap-4 ">
-              <div className="bg-[#51889D] lg:col-span-8 lg:col-start-3 flex justify-left items-center lg:row-start-2 rounded-lg h-80">
-                <p className=" font-bolder text-white lg:text-[65px]">El vende tacos</p>
-            
-                
+        <div className="bg-[#171B26] h-[80vh]">
+          <div class="lg:ml-8 lg:mr-8 ml-4 mr-4">
+            <div class="grid lg:grid-cols-12 lg:h-[748px] lg:grid-rows-6 lg:gap-x-8 lg:gap-y-0 sm:grid-cols-1 grid-cols-4  gap-4 ">
+              <div class="bg-[#51889D] lg:col-span-8 mt-4 lg:mt-0 lg:col-start-3 flex lg:row-start-2 rounded-lg lg:h-80 h-[140px]   items-center col-span-4 col-start-1 w-full">
+                <p class="font-bolder text-white lg:text-[5vw] lg:w-[60%]  text-center w-[60%] text-[25px]">
+                  El vende tacos
+                </p>
+
+                <div class="w-[40%] lg:h-full flex flex-col ">
+                  <p class="text-white flex lg:text-[7vw] justify-center items-end h-[65%] lg:h-[75%] text-[25px] font-bolder">
+                    $0.90
+                  </p>
+                  <p class="text-white lg:text-[1.5vw]  font-bolder justify-center h-[35%] lg:h-[15%] flex items-start text-[10]">
+                    {" "}
+                    Saldo disponible{" "}
+                  </p>
+                </div>
               </div>
-              <div className="lg:col-span-2 lg:col-start-9 lg:row-start-2 items-center flex h-80">
-                <p className=" text-white  flex  text-left lg:text-[90px] font-bolder ">$0.90 </p>
-                </div>
-                <div className="lg:col-span-3 lg:col-start-9 lg:row-start-2 flex ">
-                <p className="text-white lg:text-[30px] font-bolder text-left"><br /><br /><br /> <br /><br /> Saldo disponible </p>
-                </div>
-                <div className="lg:col-span-2 lg:col-start-3 lg:row-start-5 items-center flex bg-[#51889D] rounded-lg">
-                <img className="h-4/6 w-full" src={moneda} alt="not found"/>
-                </div>
-                <div className="lg:col-span-2 lg:col-start-6 lg:row-start-5 items-center flex bg-[#51889D] rounded-lg">
-                <img className="h-4/6 w-full" src={carro} alt="not found"/>
-                </div>
-                <div className="lg:col-span-2 lg:col-start-9 lg:row-start-5 items-center flex bg-[#51889D] rounded-lg">
-                <img className="h-4/6 w-full" src={tacha} alt="not found"/>
-                </div>                 
+
+              <div class="lg:col-span-2 lg:col-start-3 col-start-1 col-span-4 lg:row-start-5 items-center flex bg-[#51889D] rounded-lg h-[60px]">
+                <img
+                  onClick={hola}
+                  class="lg:h-4/6 h-3/4 w-full lg:w-full "
+                  src={moneda}
+                  alt="not found "
+                />
+              </div>
+              <div class="lg:col-span-2 lg:col-start-6 col-start-1 col-span-4 lg:row-start-5 items-center flex bg-[#51889D] rounded-lg h-[60px]">
+                <img
+                  onClick={hola2}
+                  class="lg:h-4/6 h-3/4 w-full lg:w-full"
+                  src={carro}
+                  alt="not found"
+                />
+              </div>
+              <div class="lg:col-span-2 lg:col-start-9 col-start-1 col-span-4 lg:row-start-5 items-center flex bg-[#51889D] rounded-lg h-[60px]">
+                <img
+                  onClick={hola3}
+                  class="lg:h-4/6 h-3/4 w-full lg:w-full"
+                  src={tacha}
+                  alt="not found"
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -48,5 +82,4 @@ function Home() {
     </>
   );
 }
-
 export default Home;
